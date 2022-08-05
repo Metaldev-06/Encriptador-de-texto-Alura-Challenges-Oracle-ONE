94564e0c4922946d9e0e26d0fee6ceb6bbf4be24
Fernando Diaz <fernydiaz62@gmail.com>
2022-08-05 15:30:51 -0300

    Versión 0.1
    Estructura y diseño finalizado

---
+++ js/main.js
@@ -0,0 +1,14 @@
+//! La letra "e" es convertida para "enter"
+//! La letra "i" es convertida para "imes"
+//! La letra "a" es convertida para "ai"
+//! La letra "o" es convertida para "ober"
+//! La letra "u" es convertida para "ufat"
+
+const input = document.getElementById('inputEncriptar');
+const encriptar = document.getElementById('encriptar');
+const desencriptar = document.getElementById('desencriptar');
+const copiar = document.getElementById('copiar');
+
+encriptar.addEventListener('click', () => {
+
+});
